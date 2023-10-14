499954684229e223c78dc90eae3180e820d6f761
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2023-10-14 06:37:15 +0530

    stack problems

--- Stack/generateParenthesis.js
+++ Stack/generateParenthesis.js
@@ -1,3 +1,13 @@
-// TRYAGAIN
+function generateParenthesis(n) {
+  function find(s, opens, closes) {
+    if (s.length === 2 * n) result.push(s);
 
-function generateParenthesis(n) {}
+    if (opens < n) find(s + "(", opens + 1, closes);
+    if (closes < opens) find(s + ")", opens, closes + 1);
+  }
+
+  const result = [];
+  find("", 0, 0);
+
+  return result;
+}

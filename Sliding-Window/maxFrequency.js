9328ef0cd1680a6a6d2de90a8d311993d20104df
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2024-02-29 07:58:43 +0530

    sliding window problem

--- Sliding-Window/maxFrequency.js
+++ Sliding-Window/maxFrequency.js
@@ -1,3 +1,41 @@
 // TRYAGAIN
 
-function maxFrequency(nums, k) {}
+function maxFrequency(nums, k) {
+  nums.sort((a, b) => a - b);
+  let i = 0;
+  let j = 0;
+  let result = 0;
+  const prefix = [];
+  let sum = 0;
+
+  for (const num of nums) {
+    sum += num;
+
+    prefix.push(sum);
+  }
+
+  while (j < nums.length) {
+    while (j < nums.length) {
+      if (
+        prefix[j] - (i - 1 >= 0 ? prefix[i - 1] : 0) + k <
+        nums[j] * (j - i + 1)
+      )
+        break;
+
+      result = Math.max(result, j - i + 1);
+      j++;
+    }
+
+    while (j < nums.length && i <= j) {
+      if (
+        prefix[j] - (i - 1 >= 0 ? prefix[i - 1] : 0) + k >=
+        nums[j] * (j - i + 1)
+      )
+        break;
+
+      i++;
+    }
+  }
+
+  return result;
+}
